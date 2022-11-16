e9c25f65d468a0cc23ce160f82be6feed2938c94
unaldutlu <unaldutlu@gmail.com>
2022-11-17 00:45:04 +0300

    bugs fix on page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Card from "./components/Card";
 import Header from "./components/Header";
+import data from "./data/data";
 
 function App() {
   return (
     <div className='App'>
       <Header />
-      <Card />
+      <div className='frame pb-2 mb-5 pt-2'>
+        {data.map((item, i) => {
+          const { name, img, options } = item;
+          return <Card key={i} name={name} img={img} options={options} />;
+        })}
+      </div>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,36 +1,32 @@
 import { useState } from "react";
-import data from "../data/data";
 
-function Card() {
+function Card({ name, img, options }) {
   const [card, setCard] = useState(true);
 
   const onClick = () => {
     setCard(!card);
   };
+
   return (
-    <div className='frame pb-2 mb-5 pt-2' onClick={onClick}>
-      {data.map((e, i) => (
-        <div key={i} className='cards my-1 my-md-2'>
-          {card ? (
-            <div className='text-center h-100 bg-info '>
-              <div className='py-2'>
-                <img src={e.img} alt='' className='image img-fluid' />
-              </div>
-              <div className='bg-warning d-flex justify-content-center align-items-center'>
-                <p>{e.name}</p>
-              </div>
-            </div>
-          ) : (
-            <div className=' h-100 d-flex justify-content-start align-items-center'>
-              <ul className=''>
-                <li>{e.options[0]}</li>
-                <li>{e.options[1]}</li>
-                <li>{e.options[2]}</li>
-              </ul>
-            </div>
-          )}
+    <div className='cards my-1 my-md-2' onClick={onClick}>
+      {card ? (
+        <div className='text-center h-100 bg-info'>
+          <div className='py-2'>
+            <img src={img} alt='' className='image img-fluid' />
+          </div>
+          <div className='bg-warning d-flex justify-content-center align-items-center'>
+            <p>{name}</p>
+          </div>
         </div>
-      ))}
+      ) : (
+        <div className=' h-100 d-flex justify-content-start align-items-center'>
+          <ul className=''>
+            <li>{options[0]}</li>
+            <li>{options[1]}</li>
+            <li>{options[2]}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
